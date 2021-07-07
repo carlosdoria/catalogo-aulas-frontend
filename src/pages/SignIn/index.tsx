0e96eef8373f562730c0e49f29534cd461c3548f
carlosdoria <carlosdoria953@gmail.com>
2021-07-07 17:52:10 -0300

    feat: create pages SignIn and SignUp

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styles'
+
+export default function SignIn () {
+  return (
+    <S.Container>
+      <h1>SignIn</h1>
+    </S.Container>
+  )
+}
---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styles'
+
+export default function SignUp () {
+  return (
+    <S.Container>
+      <h1>SignUp</h1>
+    </S.Container>
+  )
+}
